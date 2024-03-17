cb946fc9a4b431dab83205c30bf9951807bf2b9e
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-03-17 10:36:32 +0300

    feat: Defining a function Author

---
+++ Author.py
@@ -0,0 +1,10 @@
+# Author.py
+
+class Author:
+    def __init__(self, name):
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string")
+        self._name = name
+        self._articles = []
+
+   
