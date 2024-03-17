270628453006d0aee1c6a55970d6b5828f0dec63
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-03-17 10:49:07 +0300

    feat: Defining a function Magazine

--- Author.py
+++ Author.py
@@ -47,6 +47,20 @@ class Author:
     @property
     def magazine(self):
         return self._magazine
+    
+    class Magazine:
+    _all_magazines = []
+
+    def __init__(self, name, category):
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise ValueError("Name must be a string between 2 and 16 characters")
+        if not isinstance(category, str) or len(category) == 0:
+            raise ValueError("Category must be a non-empty string")
+        self._name = name
+        self._category = category
+        self._articles = []
+        Magazine._all_magazines.append(self)
+
 
 
 
