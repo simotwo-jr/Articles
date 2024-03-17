aa721d3161ff64d516f99fe216de26f06ff08d02
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-03-17 10:46:52 +0300

    feat: Defining a function Article

--- Author.py
+++ Author.py
@@ -23,6 +23,18 @@ class Author:
 
     def topic_areas(self):
         return list(set(article.magazine.category for article in self._articles))
+    
+    class Article:
+    def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            raise ValueError("Author must be an instance of Author")
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Magazine must be an instance of Magazine")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be a string between 5 and 50 characters")
+        self._author = author
+        self._magazine = magazine
+        self._title = title
 
 
 
