2337bf2a5ae0f9cf372413bdd3bf97441ba4cd1e
Nehemiah <nehemiahcheruiyot7069@gmail.com>
2024-03-17 10:45:38 +0300

    feat: Adding the necessary deliverables

--- Author.py
+++ Author.py
@@ -7,4 +7,25 @@ class Author:
         self._name = name
         self._articles = []
 
+         def name(self):
+        return self._name
+
+    def articles(self):
+        return self._articles
+
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
+
+    def add_article(self, magazine, title):
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
+
+    def topic_areas(self):
+        return list(set(article.magazine.category for article in self._articles))
+
+
+
+        
+
    
